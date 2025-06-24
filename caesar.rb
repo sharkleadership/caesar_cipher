e273e3478185b01220b8521ad589196e3d25f257
JM <143664295+sharkleadership@users.noreply.github.com>
2025-06-24 18:21:47 -0500

    Refactor to RuboCop standards

--- caesar.rb
+++ caesar.rb
@@ -1,26 +1,21 @@
 #!/usr/bin/env ruby
-LETTERS = %w[abcdefghijklmnopqrstuvwxyz][0].split("")
+LETTERS = %w[abcdefghijklmnopqrstuvwxyz][0].split('')
 def caesar_cipher(string, shift_length)
-  new_string = ""
-  shift_length = shift_length % 26
-  string.each_char do |char|
-    if LETTERS.none?(char.downcase)
-      new_string << char
-      next
+  shift_length %= 26
+  string.split('').map do |char|
+    next char if LETTERS.none?(char.downcase)
+
+    LETTERS.each_with_index do |let, idx|
+      next unless let == char.downcase
+
+      idx = (idx + shift_length) - 26
+      break (char == char.upcase ? LETTERS[idx].upcase : LETTERS[idx])
     end
-    LETTERS.each_with_index do |let,idx|
-      if let == char.downcase
-        idx = (idx + shift_length) - 26
-        new_string << (char == char.upcase ? LETTERS[idx].upcase : LETTERS[idx])
-        break
-      end
-    end
-  end
-  new_string
+  end.join('')
 end
 
-print "Please input a string to be run through the Caesar cipher: "
+print 'Please input a string to be run through the Caesar cipher: '
 string = gets.chomp.to_s
-print "Please input the number the string should be Caesar ciphered by: "
+print 'Please input the number the string should be Caesar ciphered by: '
 shift = gets.chomp.to_i
 puts caesar_cipher(string, shift)
